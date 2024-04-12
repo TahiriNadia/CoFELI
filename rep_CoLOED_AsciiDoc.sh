6c166d0c3829d039c61f27c6b3826f207af73d87
Nadia Tahiri <Nadia.Tahiri@USherbrooke.ca>
2024-04-12 06:04:22 -0400

    Add files via upload

---
+++ rep_CoLOED_AsciiDoc.sh
@@ -0,0 +1,10 @@
+# Contexte de support requis par la composante AsciiDoc de CoLOED
+export rep_racine=/Users/Shared/DGS
+export rep_CoLOED=${rep_racine}/CoLOED
+export rep_AsciiDoc=${rep_CoLOED}/AsciiDoc
+export rep_format=${rep_AsciiDoc}/format
+
+echo "rep_racine = ${rep_racine}"
+echo "rep_CoLOED = ${rep_CoLOED}"
+echo "rep_AsciiDoc = ${rep_AsciiDoc}"
+echo "rep_format = ${rep_format}"
